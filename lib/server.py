297bbff4dcc10831a109f997e080cddc985cffda
Jusufadis Bakamovic <jbakam@gmail.com>
2019-03-12 11:31:59 +0100

    Integrate the code-completion plugin into the server instantiation.

--- lib/server.py
+++ lib/server.py
@@ -3,6 +3,7 @@ from services.clang_format.clang_format import VimClangFormat
 from services.clang_tidy.clang_tidy import VimClangTidy
 from services.project_builder.project_builder import VimProjectBuilder
 from services.source_code_model.source_code_model import VimSourceCodeModel
+from services.code_completion.code_completion import VimCodeCompletion
 
 def get_instance(handle, project_root_directory, target_configuration, args):
     vim_instance = args
@@ -13,5 +14,6 @@ def get_instance(handle, project_root_directory, target_configuration, args):
         VimSourceCodeModel(vim_instance),
         VimProjectBuilder(vim_instance),
         VimClangFormat(vim_instance),
-        VimClangTidy(vim_instance)
+        VimClangTidy(vim_instance),
+        VimCodeCompletion(vim_instance)
     )
